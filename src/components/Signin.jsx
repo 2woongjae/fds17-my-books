0732aee3e0e08a92f521d8a2039b4c025d92c899
Mark Lee <mark@protopie.io>
2020-12-22 21:02:13 +0900

    12월 22일 수업 끝

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col, Input, Button } from 'antd';
 import styles from './Signin.module.css';
+import axios from 'axios';
 
 // class 컴포넌트에서 사용하는 createRef 함수
 class Signin extends React.Component {
@@ -11,12 +12,9 @@ class Signin extends React.Component {
   };
 
   render() {
-    console.log(this.state.email);
     const isEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(
       this.state.email,
     );
-
-    console.log(this._password);
 
     return (
       <form>
@@ -41,31 +39,26 @@ class Signin extends React.Component {
                   <span className={styles.required}> *</span>
                 </div>
                 <div className={styles.input_area}>
-                  {/* <Input
-                          name="email"
-                          placeholder="Email"
-                          autoComplete="email"
-                          className={styles.input}
-                        /> */}
-                  <input
-                    type="text"
+                  <Input
+                    name="email"
+                    placeholder="Email"
+                    autoComplete="email"
+                    className={styles.input}
                     value={this.state.email}
                     onChange={this.change}
-                    onMouseOver={this._onMouseOver}
                   />
-                  {isEmail ? '이메일 맞음' : '이메일 아님'}
                 </div>
                 <div className={styles.email_title}>
                   Password
                   <span className={styles.required}> *</span>
                 </div>
                 <div className={styles.input_area}>
-                  {/* <Input
+                  <Input
                     type="password"
                     autoComplete="current-password"
                     className={styles.input}
-                  /> */}
-                  <input type="password" ref={this._password} />
+                    ref={this._password}
+                  />
                 </div>
                 <div className={styles.button_area}>
                   <Button
@@ -73,6 +66,7 @@ class Signin extends React.Component {
                     loading={false}
                     className={styles.button}
                     onClick={this.click}
+                    disabled={!isEmail}
                   >
                     Sign In
                   </Button>
@@ -85,13 +79,36 @@ class Signin extends React.Component {
     );
   }
 
-  _onMouseOver = (e) => {
-    this._password.current.focus();
-  };
+  click = async () => {
+    const { email } = this.state;
+    const password = this._password.current.input.value;
+    console.log('clicked', email, password);
 
-  click = () => {
-    const password = this._password.current.value;
-    console.log('clicked', this.state.email, password);
+    // 서버에다가 이메일 패스워드 보내서 인증된 사용자인지 체크
+    // axios
+    //   .post('https://api.marktube.tv/v1/me', {
+    //     email,
+    //     password,
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    try {
+      // 호출 시작 => 로딩 시작
+      const response = await axios.post('https://api.marktube.tv/v1/me', {
+        email,
+        password,
+      });
+      // 호출 완료 (정상) => 로딩 끝
+      console.log(response);
+    } catch (error) {
+      // 호출 완료 (에러) => 로딩 끝
+      console.log(error);
+    }
   };
 
   change = (e) => {
