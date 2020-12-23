0d5b5c373a451fb14d1f2b7ee793518f56b3b23c
Mark Lee <mark@protopie.io>
2020-12-23 20:12:54 +0900

    12월 23일 3교시 중

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import { sleep } from '../utils';
+import { LoadingOutlined } from '@ant-design/icons';
 
 export default class BookList extends React.Component {
   state = {
     books: [],
+    loading: false,
   };
 
   render() {
-    const { books } = this.state;
+    const { books, loading } = this.state;
 
     return (
       <div>
-        <h1>Book List</h1>
+        <h1>Book List {loading && <LoadingOutlined />}</h1>
         <ul>
           {books.map((book) => {
             return <li>{book.title}</li>;
@@ -23,14 +26,16 @@ export default class BookList extends React.Component {
 
   async componentDidMount() {
     // 서버에 책 리스트 다오.
+    this.setState({ loading: true });
     const response = await axios.get('https://api.marktube.tv/v1/book', {
       headers: {
         Authorization: `Bearer ${this.props.token}`,
       },
     });
 
-    console.log(response);
+    await sleep(2000);
 
     // 받은 책 리스트로 다시 랜더 해줘 <= state
+    this.setState({ books: response.data, loading: false });
   }
 }
