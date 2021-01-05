4ac02a60d1b81d7f40d052a90070c3ed0e3f0893
Mark Lee <mark@protopie.io>
2021-01-05 17:46:59 +0900

    redux-actions

--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -1,39 +1,42 @@
 import { push } from 'connected-react-router';
+import { createActions, handleActions } from 'redux-actions';
 import { delay, put, takeEvery, call } from 'redux-saga/effects';
 import AuthService from '../../services/AuthService';
 
-// namespace
-const namespace = 'fds17-my-books/auth';
+// prefix
+const prefix = 'fds17-my-books/auth';
 
-// action types
-const START = namespace + '/START';
-const SUCCESS = namespace + '/SUCCESS';
-const FAIL = namespace + '/FAIL';
+// action creator
+const { start, success, fail } = createActions('START', 'SUCCESS', 'FAIL', {
+  prefix,
+});
 
 // initial state
 const initialState = { token: null, loading: false, error: null };
 
 // reducer
-export default function auth(state = initialState, action) {
-  switch (action.type) {
-    case START:
-      return { token: null, loading: true, error: null };
-    case SUCCESS:
-      return { token: action.token, loading: false, error: null };
-    case FAIL:
-      return { token: null, loading: false, error: action.error };
-    default:
-      return state;
-  }
-}
+const auth = handleActions(
+  {
+    START: () => ({ token: null, loading: true, error: null }),
+    SUCCESS: (state, action) => ({
+      token: action.payload,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action) => ({
+      token: null,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  { prefix },
+);
 
-// action creator
-export const signinStart = () => ({ type: START });
-export const signinSuccess = (token) => ({ type: SUCCESS, token });
-export const signinFail = (error) => ({ type: FAIL, error });
+export default auth;
 
 // saga
-const SIGNIN_SAGA = namespace + '/SIGNIN_SAGA';
+const SIGNIN_SAGA = prefix + '/SIGNIN_SAGA';
 export const signinSagaStart = (email, password) => ({
   type: SIGNIN_SAGA,
   payload: { email, password },
@@ -41,7 +44,7 @@ export const signinSagaStart = (email, password) => ({
 
 export function* signinSaga(action) {
   try {
-    yield put(signinStart());
+    yield put(start());
 
     const { email, password } = action.payload;
 
@@ -49,11 +52,11 @@ export function* signinSaga(action) {
     yield delay(2000);
 
     localStorage.setItem('token', token); // 토큰을 브라우저 어딘가에 저장한다.
-    yield put(signinSuccess(token));
+    yield put(success(token));
     yield put(push('/'));
   } catch (error) {
     console.log(error);
-    yield put(signinFail(error));
+    yield put(fail(error));
   }
 }
 
